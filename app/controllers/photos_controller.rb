c18c5ff6acc02b2038ccafa4db14a3796b0f6b84
KeithM15 <keith.martin@iomart.com>
2019-08-14 18:58:37 +0100

    adds actions for photo uploads

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,27 @@
+class PhotosController < ApplicationController
+  def index
+    respond_to do |format|
+      format.json  { render :json => Photo.all}
+    end
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    if @photo.save
+      flash[:notice] = 'Successfully added photo'
+      redirect_to :root
+    else
+      flash[:error] = 'Unable to add photo'
+      redirect_to :back
+    end
+  end
+
+  private 
+  def photo_params
+    params.require(:photo).permit(:image,:lat, :lng)
+  end
+end
