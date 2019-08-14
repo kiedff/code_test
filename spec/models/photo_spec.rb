451dc15a6dd58d6d8ad234f69ceea81570dc2d4e
KeithM15 <keith.martin@iomart.com>
2019-08-14 15:58:00 +0100

    adds some basic tests

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Photo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Photo, type: :model do 
+  let(:photo) { create :photo }
+
+  it 'has a latitude' do
+    expect(photo.lat).to eq 1.5
+  end
+
+  it 'has a longitude' do
+    expect(photo.lng).to eq 1.5
+  end
+
+  it 'has an image' do
+    expect(photo.image).to eq 'MyString'
+  end
 end
