73278887658801f2f1413b28311610adadd8b7ad
KeithM15 <keith.martin@iomart.com>
2019-08-16 21:09:58 +0100

    fixes typo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
       :provider => 'AWS',
       :aws_access_key_id => ENV["AWS_ACCESS_KEY_ID"],
       :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
-      :region => 'eu-west-1'
+      :region => 'eu-west-2'
 }
 
 config.fog_directory = ENV['AWS_BUCKET_NAME']
