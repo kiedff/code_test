bab6507b2df54b8a721148ca9484a70f81841fbd
KeithM15 <keith.martin@iomart.com>
2019-08-16 21:23:03 +0100

    fixing storage type

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
-   storage :fog
+   # storage :fog
+   storage :aws
   else
    storage :file
   end
