301f770217c578e981b5a4c11ae4391f4ac45953
KeithM15 <keith.martin@iomart.com>
2019-08-16 22:05:42 +0100

    removes unused line

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,9 @@
 CarrierWave.configure do |config|
-  config.storage = Rails.env.production? ? :fog : :file
-
   config.fog_credentials = {
     provider:              'AWS',  
     aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],       
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], 
-    region:                'eu-west-2',  
-    host:                  's3.example.com'
+    region:                'eu-west-2'
   }
   config.fog_directory = ENV['S3_BUCKET'] 
   config.fog_public = false
