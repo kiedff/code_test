26b030fb13246871a37d4d1960f23debab79cb84
KeithM15 <keith.martin@iomart.com>
2019-08-16 20:24:35 +0100

    removes image validation as breaks seeding

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
   mount_uploader :image, ImageUploader
 
-  validates :lat, :lng, :image, presence: true
+  validates :lat, :lng, presence: true
   validates :lat, :lng, numericality: true
 end
