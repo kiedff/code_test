3861edc1e4b16c0b551ab4821458297d94d3594a
KeithM15 <keith.martin@iomart.com>
2019-08-16 21:48:54 +0100

    trying workaround

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,7 @@
 CarrierWave.configure do |config|
+  config.storage = Rails.env.production? ? :fog : :file
+  config.fog_directory  = ENV['S3_BUCKET']  # required
+  config.fog_public  = false  # optional, defaults to true
 
   config.fog_credentials = {
     :provider  => 'AWS',  # required
@@ -6,7 +9,6 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],  # required
     :region => 'eu-west-2',  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = ENV['S3_BUCKET']  # required
-  config.fog_public  = false  # optional, defaults to true
-  config.storage = Rails.env.production? ? :fog : :file
+  
+  
 end
