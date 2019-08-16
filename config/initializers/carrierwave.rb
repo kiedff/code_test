2d134ba4ec717ae97dd0b139c89f7a251683fbf7
KeithM15 <keith.martin@iomart.com>
2019-08-16 22:16:50 +0100

    so it runs locally without aws creds

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,15 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_directory = ENV['S3_BUCKET']
+  if Rails.env.production? 
+    config.fog_provider = 'fog/aws'
+    config.fog_directory = ENV['S3_BUCKET']
 
-  config.fog_credentials = {
-    provider:              'AWS',  
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],       
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], 
-    region:                'eu-west-2'
-  }
+    config.fog_credentials = {
+      provider:              'AWS',  
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],       
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], 
+      region:                'eu-west-2'
+    }
+  end
 end
 
  
